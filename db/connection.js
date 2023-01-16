f1b9cab3925d89db802a43b1d0177c328e9c76c3
willbrennana <will.brennana@gmail.com>
2023-01-16 12:02:54 -0600

    Installed json package, added dependencies, installed npm and added pseudocode for index.js files.

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const connection = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`),
+  connection.connect(function (err) {
+    if (err) throw err;
+  })
+);
+
+module.exports = connection;
